1d193c63b7266bf7c188f956b7f535af30f72be7
mnzarria <mnzarria@gmail.com>
2023-05-13 19:13:42 -0300

    Mejora en agregado de productos al carrito

--- Pre-entrega1/src/CartManager.js
+++ Pre-entrega1/src/CartManager.js
@@ -31,31 +31,39 @@ class CartManager {
     }
   }
 
-  async addProductToCart(idCart, idProduct) {
-    if (!idProduct || !idCart) return "Faltan datos";
+  async addProductToCart(cid, pid) {
+    if (!pid || !cid) return "Faltan datos";
 
     const carts = await this.#getCarts();
-    const cart = carts.find((cart) => cart.id === idCart);
-    if (!cart) return "Cart does not exist";
-
-    const prod = await productManager.getProductById(idProduct);
-    if (!prod) {
-      return null;
+    const cart = carts.find((cart) => cart.id === cid);
+    //Caso A: se intenta editar un carrito que no existe
+    if (!cart) {
+      return "El carrito no existe"
     }
-
+    //Caso B: se intenta agregar al carrito un producto que aun no existe en la base de datos
+    const prod = await productManager.getProductById(pid);
+    if (!prod) {
+      return "Producto no existente en la base de datos de productos";
+    }
+    
+    //Caso C: Carrito y productos existen en la base de datos
     //Si el producto no está, lo agrego. Si ya está, aumento la cantidad
-
     const product = cart.products.find(
-      (product) => product.idProduct === idProduct
+      (product) => product.pid === pid
     );
+    //Si el producto no existe dentro del carrito, coloco la primera unidad
     if (!product) {
-      cart.products.push({ idProduct, quantity: 1 });
-    } else {
+      cart.products.push({ pid, quantity: 1 });
+    } 
+    //Si el producto existe dentro del carrito, aumento la cantidad
+    else {
       product.quantity++;
     }
-
+    //Actualizo al carrito y lo guardo en archivo
+    const cartIndex = carts.findIndex((c) => c.id === cid);
+    carts.splice(cartIndex, 1, cart);
     await fs.promises.writeFile(this.path, JSON.stringify(carts));
-    return cart;
+    return "Producto agregado al carrito";
   }
 
   async #getCarts() {
